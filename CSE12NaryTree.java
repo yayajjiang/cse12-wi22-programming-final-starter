2156ef112f19ed4da6382d15404f9757c48dc95c
Andrew Tang <32887375+tangandrew@users.noreply.github.com>
2022-03-10 13:13:17 -0800

    Update CSE12NaryTree.java

--- CSE12NaryTree.java
+++ CSE12NaryTree.java
@@ -19,6 +19,7 @@ public class CSE12NaryTree<E extends Comparable<E>> {
     
     /**
      * This inner class encapsulates the data and children for a Node.
+     * Do NOT edit this inner class.
      */
     protected class Node{
         E data;
